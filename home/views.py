001107bce151bf4d1b360dbaa906f5f8ea2039ea
junjar singh <junjar.singh7035@gmail.com>
2023-09-16 15:27:18 +0530

    added  carasoul and ice cream listing grid

--- home/views.py
+++ home/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    context={
-        'variable':"this is sent"
-    }
-    return render(request,"index.html",context )
+   
+    return render(request,"base.html" )
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request,"about.html" )
 def services(request):
-    return HttpResponse("this is services page")
+    return render(request,"services.html" )
 def contact(request):
-    return HttpResponse("this is contact page")
+    return render(request,"contact.html" )
