cf52616579c9715616254b357f9cb90de25cbd36
junjar singh <junjar.singh7035@gmail.com>
2023-09-17 21:41:27 +0530

    created contact page and saved those details in db and improve content in home page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from home.models import Contact1
+admin.site.register(Contact1)
 
 # Register your models here.
--- home/views.py
+++ home/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact1
+from django.contrib import messages
 # Create your views here.
 def index(request):
-   
+    
     return render(request,"base.html" )
 def about(request):
     return render(request,"about.html" )
 def services(request):
     return render(request,"services.html" )
 def contact(request):
-    return render(request,"contact.html" )
+    
+    if request.method=="POST":
+            name=request.POST.get('name')
+            email=request.POST.get("email")
+            phone=request.POST.get("phone")
+            desc=request.POST.get("desc")
+            contact=Contact1(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+            contact.save()
+            messages.success(request, "Profile details updated.")
+    return render(request,"contacts1.html" )
